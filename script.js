0fbd37ca7f18a497512306dc6a5c32804ae5b3ca
imjopa <joaopaulocosta.r@hotmail.com>
2023-09-28 11:46:46 -0300

    inicio do movimento da cobrinha

--- script.js
+++ script.js
@@ -10,17 +10,42 @@ snake[0] = {
 
 function criarBG() {
 
-context.fillStyle = "lightgreen";
-context.fillRect(0, 0, 16 * box, 16 * box);
+    context.fillStyle = "lightgreen";
+    context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 
 function criarCobrinha() {
-    for( i = 0; i < snake.length; i++) { 
-          context.fillStyle = "green";
-          context.fillRect(snake[i].x, snake[i].y, box, box); 
+    for (i = 0; i < snake.length; i++) {
+        context.fillStyle = "green";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-criarBG( );
-criarCobrinha();
+function iniciarJogo() {
+    criarBG();
+    criarCobrinha();
+
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if(direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
+
+    snake.pop();
+
+    let newHead ={
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead);
+
+}
+
+
+
+// CRIA UM INTERVALO DE 100 MILISEGUNDO PARA A FUNÇÃO INICIAR JOGO RENOVAR A CADA 100 MILISEGUNDOS PARA NÃO TRAVAR
+let jogo = setInterval(iniciarJogo, 100);
