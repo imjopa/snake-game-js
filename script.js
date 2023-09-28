89910b595111b8ea03b88141099c6899f102bad5
imjopa <joaopaulocosta.r@hotmail.com>
2023-09-28 17:13:32 -0300

    draw food

--- script.js
+++ script.js
@@ -8,20 +8,27 @@ snake[0] = {
 }
 
 let direction = "right";
-
+let food = {
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box 
+}
 
 function criarBG() {
-
-    context.fillStyle = "lightgreen";
+    context.fillStyle = "green";
     context.fillRect(0, 0, 16 * box, 16 * box);
 }
 
 
 function criarCobrinha() {
     for (i = 0; i < snake.length; i++) {
-        context.fillStyle = "green";
+        context.fillStyle = "lightgreen";
         context.fillRect(snake[i].x, snake[i].y, box, box);
     }
+}
+
+function drawFood(){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
 document.addEventListener('keydown', update)
@@ -41,6 +48,7 @@ function iniciarJogo() {
     
     criarBG();
     criarCobrinha();
+    drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
